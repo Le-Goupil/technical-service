4ba9bfe04d7ae29cad657070a5e23b456d9a2738
Le-Goupil <apmonteiropro@gmail.com>
2022-06-20 01:07:07 +0200

    [MODIFY] import doc/setDoc from firestore

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { doc, setDoc } from "firebase/firestore";
 
 export default function Login() {
   const [email, setEmail] = useState("");
