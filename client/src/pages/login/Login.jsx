c1fdc3e080043b6306a57de33aaaf0f3a68c2d91
Le-Goupil <apmonteiropro@gmail.com>
2022-06-20 00:36:24 +0200

    [MODIFY] rename id to fit a rule in styling so no global css

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,25 +1,52 @@
 import React from "react";
 import "./login.css";
 import logo from "../../assets/everping_logo.png";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
   const handleLogin = (e) => {
     e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
   };
 
   return (
     <div id="login">
-      <form onSubmit={handleLogin}>
-        <div id="logo">
-          <img src={logo} alt="Logo de Everping" />
+      <form id="form-login" onSubmit={handleLogin}>
+        <div id="logo-login">
+          <img id="img-login" src={logo} alt="Logo de Everping" />
         </div>
-        <h1>Login to Everping App</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <h1 id="h1-login">Login to Everping App</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Connexion</button>
       </form>
       <p>
-        Vous n'avez pas de compte ? <span>Créer un compte</span>
+        Vous n'avez pas de compte ? <span id="span-login" onClick={() => navigate("/register")}>Créer un compte</span>
       </p>
     </div>
   );
