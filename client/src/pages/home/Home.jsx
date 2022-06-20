8b86840e0a036e44037a5600a0da697c532ce634
Le-Goupil <apmonteiropro@gmail.com>
2022-06-21 00:54:36 +0200

    [MODIFY] add spinner waiting during undefined props, import new components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Spinner from "../../assets/Spinner.svg";
+import "./home.css";
+import ClientHome from "../../components/ClientHome";
+import TechnicienHome from "../../components/TechnicienHome";
 
-export default function Home(){
+export default function Home(props) {
+  if (props.isTechnicien === undefined) {
     return (
-        <h1>Home</h1>
-    )
+      <div id="home-spinner">
+        <img id="spinner-img-home" src={Spinner} alt="Spinner" />
+      </div>
+    );
+  } else if (props.isTechnicien) {
+    return <TechnicienHome />;
+  } else {
+    return <ClientHome />;
+  }
 }
