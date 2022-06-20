b5375a18082d42df3444dfd14bf65cf729b03bf7
Le-Goupil <apmonteiropro@gmail.com>
2022-06-21 00:52:19 +0200

    [MODIFY] Fetch DB to get userType (technicien or not)

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,31 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
+import { useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+import { useEffect } from "react";
 
 function App() {
+  const [isTechnicien, setIsTechnicien] = useState();
   const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchUserType = async (user) => {
+      const docRef = doc(db, "user", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const userRef = docSnap.data();
+        if (userRef.technicien) {
+          setIsTechnicien(true);
+        } else {
+          setIsTechnicien(false);
+        }
+      }
+    };
+    fetchUserType(currentUser);
+  }, [currentUser]);
 
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/login" />;
@@ -30,7 +52,7 @@ function App() {
             index
             element={
               <RequireAuth>
-                <Home />
+                <Home isTechnicien={isTechnicien} />
               </RequireAuth>
             }
           />
