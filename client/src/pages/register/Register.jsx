ee18b0ba8ed30ab48fb7ae192211778fa398d6d3
Le-Goupil <apmonteiropro@gmail.com>
2022-06-20 18:56:28 +0200

    [MODIFY] push user to DB if succes, return error message if error

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -6,9 +6,11 @@ import { auth } from "../../firebase";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function Register() {
   const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVerif, setPasswordVerif] = useState("");
   const [error, setError] = useState(false);
@@ -17,20 +19,44 @@ export default function Register() {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    if (password === passwordVerif) {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log(user);
-        })
-        .then(() => setConfirmation("Votre compte à bien été créé"))
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-        });
+    if (usernameRequirement(username)) {
+      if (password === passwordVerif) {
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            const user = userCredential.user;
+            addUserToDb(user);
+          })
+          .then(() => {
+            setConfirmation("Votre compte à bien été créé");
+            setTimeout(() => {
+              navigate("/login");
+            }, 3000);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+          });
+      } else {
+        setError("Password don't match");
+      }
     } else {
-      setError("Password don't match");
+      setError("Username incorrect");
     }
+  };
+
+  const addUserToDb = async (data) => {
+    await setDoc(doc(db, "user", data.uid), {
+      admin: false,
+      email: data.email,
+      handleTicket: [],
+      openTicket: [],
+      technicien: false,
+      username: username,
+    });
+  };
+
+  const usernameRequirement = (username) => {
+    return username.match(/^[a-z0-9_-]{3,15}$/);
   };
 
   return (
@@ -46,6 +72,11 @@ export default function Register() {
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
@@ -55,7 +86,7 @@ export default function Register() {
           placeholder="Password verification"
           onChange={(e) => setPasswordVerif(e.target.value)}
         />
-        <button type="submit">Connexion</button>
+        <button type="submit">Inscription</button>
       </form>
       <p>
         Vous avez déjà un compte ?{" "}
@@ -63,6 +94,8 @@ export default function Register() {
           Connectez-vous
         </span>
       </p>
+      {error && <span id="error-register">{error}</span>}
+      {confirmation && <span id="succes-register">{confirmation}</span>}
     </div>
   );
 }
