44fbdcbc45d4230c331c746c2e8df29cc8383a67
Le-Goupil <apmonteiropro@gmail.com>
2022-06-22 01:48:41 +0200

    [MODIFY] show ticket form onClick

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import "./home.css";
 import Spinner from "../../assets/Spinner.svg";
-import ClientHome from "../../components/ClientHome";
-import TechnicienHome from "../../components/TechnicienHome";
+import Header from "../../components/Header";
+import Sidebar from "../../components/SideBar";
+import { useState } from "react";
+import TicketForm from "../../components/TicketForm";
 
 export default function Home(props) {
-  if (props.isTechnicien === undefined) {
+  const [openTicket, setOpenTicket] = useState(false);
+  console.log(openTicket);
+  if (props.user === undefined) {
     return (
       <div id="home-spinner">
         <img id="spinner-img-home" src={Spinner} alt="Spinner" />
       </div>
     );
-  } else if (props.isTechnicien) {
-    return <TechnicienHome currentUser={props.currentUser} />;
   } else {
-    return <ClientHome currentUser={props.currentUser} />;
+    return (
+      <div className="home">
+        <div id="home-header">
+          <Header user={props.user} />
+        </div>
+        <div id="home-body">
+          <Sidebar user={props.user} setOpenTicket={setOpenTicket} />
+          {openTicket && (
+            <TicketForm setOpenTicket={setOpenTicket} user={props.user} />
+          )}
+        </div>
+      </div>
+    );
   }
 }
