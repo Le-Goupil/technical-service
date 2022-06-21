3ef64f7d703fedc5a79f64fa5ada75bf9daeaf7c
Le-Goupil <apmonteiropro@gmail.com>
2022-06-22 01:43:35 +0200

    [ADD] form for userTicket

---
+++ client/src/components/TicketForm.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./style/ticketForm.css";
+import {
+  doc,
+  serverTimestamp,
+  setDoc,
+  addDoc,
+  collection,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useState } from "react";
+
+export default function TicketForm(props) {
+  const [sujet, setSujet] = useState();
+  const [description, setDescription] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      addTicketToDb();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addTicketToDb = async () => {
+    const collectionRef = collection(db, "ticket");
+    await addDoc(collectionRef, {
+      user: props.user.id,
+      creatingDate: new Date(),
+      subject: sujet,
+      description: description,
+      messages: [],
+      status: true,
+      technicien: null,
+      room: null,
+      survey: null,
+      previousTicket: null,
+    });
+  };
+  return (
+    <>
+      <p onClick={() => props.setOpenTicket(false)}>x</p>
+      <h1>
+        Remplissez le formulaire pour entrer en contact avec un technicien.
+      </h1>
+      <form id="ticket-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Sujet"
+          onChange={(e) => setSujet(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Decrivez votre probleme"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Envoyez</button>
+      </form>
+    </>
+  );
+}
