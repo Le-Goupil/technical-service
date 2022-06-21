2e1622a5e30712c720918568c67f2423851d924a
Le-Goupil <apmonteiropro@gmail.com>
2022-06-22 01:45:51 +0200

    [MODIFY] refactor due to new user props + conditional rendering on tech & admin

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,22 +4,33 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-export default function Header() {
+export default function Header(props) {
   const { dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
-
-  return (
-    <header id="header">
-      <button
-        id="disconnect"
-        onClick={() => {
-          dispatch("LOGOUT");
-          localStorage.removeItem("user");
-          navigate("/login");
-        }}
-      >
-        Disconnect
-      </button>
-    </header>
-  );
+  if (props.user) {
+    return (
+      <header id="header">
+        <button
+          id="disconnect"
+          onClick={() => {
+            dispatch("LOGOUT");
+            localStorage.removeItem("user");
+            navigate("/login");
+          }}
+        >
+          Disconnect
+        </button>
+        {props.user.data.admin ? (
+          <a className="panel-button" href="/admin">
+            Admin Panel
+          </a>
+        ) : null}
+        {props.user.data.technicien ? (
+          <a href="/technicien" className="panel-button">
+            Technicien Panel
+          </a>
+        ) : null}
+      </header>
+    );
+  }
 }
