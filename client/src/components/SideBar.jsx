2edc4eb02a417deb09a4b05b525516a0854bcdd2
Le-Goupil <apmonteiropro@gmail.com>
2022-06-22 01:47:33 +0200

    [MODIFY] condition rendering on technicien or not

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "./style/sidebar.css";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "./firebase";
+import { db } from "../firebase";
 
-export default function Sidebar(props) {}
+export default function Sidebar(props) {
+  return (
+    <div className="sidebar">
+      {!props.user.technicien ? (
+        <button onClick={() => props.setOpenTicket(true)}>
+          Creer un ticket
+        </button>
+      ) : null}
+    </div>
+  );
+}
